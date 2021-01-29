a8ea1d9a4ec7eda300ff0661e1009dd4b80e76e2
joshsrai <joshsrai@gmail.com>
2021-01-29 14:18:31 -0500

    commit tree 1b944e29bd497f0455dfd72904f94bd86d8057b1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+// Challenge 1
+
+function fetchImages() {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => displayImages(json.message))
+  }
+  
+  function displayImages(images) {
+    let container = document.getElementById("dog-image-container")
+    images.forEach(function(i) {
+      let node = document.createElement("img")
+      node.src = i
+      container.appendChild(node)
+    })
+  }
+  window.onload = function() {
+    fetchImages()
+    fetchBreeds()
+  }
+  // -----------------------
+
+//   Challenge 2
+
+function fetchBreeds() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(resp => resp.json())
+    .then(json => displayBreeds(Object.keys(json.message)))
+  }
+
+//   Challenge 4
+
+function displayBreeds(breeds) {
+    let list = document.getElementById("dog-breeds")
+    let filter = document.getElementById("breed-dropdown")
+    filter.addEventListener("change", function(){
+      filteredBreeds = breeds.filter(function(breed) {
+        return breed.charAt(0) == filter.value
+      })
+      list.innerHTML = ''
+      filteredBreeds.forEach(function(b){
+        list.innerHTML += `<li>${b}</li>`
+      })
+    //   Challenge 3
+      let lis = document.querySelectorAll("#dog-breeds li")
+      lis.forEach(function(li){
+        li.addEventListener('click', function(){
+          li.style.color="red"
+        })
+      })
+    })
+  }
